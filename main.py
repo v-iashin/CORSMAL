6615f5b5fff5bfbce75c3c0ca5859435c92836e7
Vladimir Iashin <vdyashin@gmail.com>
2020-09-04 12:06:13 +0300

    reverted state of Submission_form; main improvm.

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import pandas as pd
-import argparse, os
-'''
-1. Selects the approaches for each task
-2. Takes prediction files from the best (you select) approach for each task
-3. Fills the `Submission.csv` columns with predictions and use these columns to estimate the final aggregated value (filling mass) -- mind the density of types (water, rice, pasta)
-'''
+import argparse
+
 
 if __name__ == "__main__":
     # Take the file paths from arguments and fill the form
-    parser=argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
     parser.add_argument('-t', '--ftype', nargs='+', help='Filling type csv file.', required=True)
     parser.add_argument('-l', '--flevel', nargs='+', help='Filling level csv file.', required=True)
     parser.add_argument('-c', '--capacity', nargs='+', help='Filling capacity csv file.', required=True)
-    parser.add_argument('-o', '--output', help='Name of the output file, w/o extension.', default='Submission_form')
+    parser.add_argument('-o', '--output', help='Name of the output file', default='./submission.csv')
 
     args = parser.parse_args()
-
 
     # get the empty submission form
     df_sub = pd.read_csv('Submission_form.csv')
@@ -31,4 +26,4 @@ if __name__ == "__main__":
     df_sub['Filling level [%]'] = df_flevel['Filling level [%]']
     df_sub['Container capacity [mL]'] = df_capacity['Container capacity [mL]']
 
-    df_sub.to_csv(args.output+'.csv', index=False)
+    df_sub.to_csv(args.output, index=False)
