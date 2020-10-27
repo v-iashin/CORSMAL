c200e7036e58db50a24b7feadc6b433408d807ec
Vladimir Iashin <vdyashin@gmail.com>
2020-10-27 16:25:44 +0200

    remove features; filepaths and preds

--- filling_level/vggish/extract_video_features.sh
+++ filling_level/vggish/extract_video_features.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+DEVICE=$1
+# the same as =$2 but removes trailing slash in the path
+DATA_ROOT=${2%/}
+
 # you may want to switch it to `anaconda3` if you install anaconda instead of miniconda
 source ~/miniconda3/etc/profile.d/conda.sh
-
-# path to corsmal dataset without trailing slash (`/`) e.g. folder should have 1/ 2/ 3/ ... 12/ folders
-DATA_ROOT="/home/nvme/vladimir/corsmal" # ---- NO TRAILING '/' i.e. not '.....mal/'
 
 # making sure we are not in 'base' or any other
 conda deactivate
 conda deactivate
-
-# make sure to install it first (follow the guidelines in `./video_features` folder in README.md)
-# make sure to download the model
 conda activate vggish
 
-# form paths for feature extraction (will be saved to ./filepaths)
-python ./form_filenames.py --data_root $DATA_ROOT
+# change dir to `./video_features` folder
+# also patching the video_features code by creating tmp dir otherwise will fail with system error
+cd ./video_features && mkdir ./tmp
 
-# moving to `./video_features` folder
-cd ./video_features
-
-for container_id in {1..12}
+for container_dir in *$DATA_ROOT/
 do
-/home/vladimir/miniconda3/envs/vggish/bin/python main.py \
+# take only the last folder in the path which is the container id
+container_id="$(basename $container_dir)"
+
+# run feature extraction
+python main.py \
     --feature_type vggish \
-    --device_ids 0 1 \
+    --device_ids $DEVICE \
     --on_extraction save_numpy \
-    --file_with_video_paths "../filepaths/"$container_id"_audio_file_paths.txt" \
+    --video_paths $(find $DATA_ROOT -name "*.wav")\
     --output_path "../vggish_features/"$container_id"/vggish"
 done
