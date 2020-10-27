622eee7123836008a4c8d288921faca60ea1c66e
Vladimir Iashin <vdyashin@gmail.com>
2020-10-27 22:03:01 +0200

    a follow-up

--- filling_level/vggish/extract_features.sh
+++ filling_level/vggish/extract_features.sh
@@ -2,6 +2,7 @@
 DEVICE=$1
 # the same as =$2 but removes trailing slash in the path
 DATA_ROOT=${2%/}
+FIRST_EVAL_CONTAINER=$3
 
 # you may want to switch it to `anaconda3` if you install anaconda instead of miniconda
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
@@ -19,6 +20,11 @@ for container_dir in $DATA_ROOT/*
 do
 # take only the last folder in the path which is the container id
 container_id="$(basename $container_dir)"
+
+# skip if less than FIRST_EVAL_CONTAINER
+if (( container_id < FIRST_EVAL_CONTAINER )); then
+continue
+fi
 
 # run feature extraction
 python main.py \
